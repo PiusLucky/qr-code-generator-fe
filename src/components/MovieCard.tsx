47cfad24a84e07d570039913900603351a16b7b5
PiusLucky <luckypius50@gmail.com>
2023-10-10 19:42:23 +0100

    chore: update movie card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -11,7 +11,7 @@ function MovieCard({ id, title, year, image }: IProps) {
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-8 mr-8">
       <img className="rounded-t-lg" src={image} alt="" />
       <div className="p-5">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <h5 className="mb-2 md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
         </h5>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
